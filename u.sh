6f084c83186f5ac738b50f1c0c693c9e387b2545
Done4us <77003768+done4us@users.noreply.github.com>
2021-08-04 22:11:15 +0530

    Update u.sh

--- u.sh
+++ u.sh
@@ -1,7 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
 mv update //data/data/com.termux/files/usr/bin
+mv z //data/data/com.termux/files/usr/bin
 cd /data/data/com.termux/files/usr/bin
 chmod +x update
+chmod +x z
 cd /data/data/com.termux/files/home
 cd error
 bash update.sh
